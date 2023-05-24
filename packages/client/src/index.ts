d449b719425d8e2a30cade8eb1aa8ef37543962a
wighawag <wighawag@gmail.com>
2023-05-24 17:09:36 +0100

    private key 1

--- packages/client/src/index.ts
+++ packages/client/src/index.ts
@@ -5,14 +5,12 @@ import {createWallet} from './ViemWallet';
 
 const provider = new JSONRPCHTTPProvider('http://localhost:8545');
 const db = createInMemoryKeyValueDB();
-
 const time = {
 	async getTimestamp() {
 		return Math.floor(Date.now() / 1000);
 	},
 };
-
-const privateKey = '0x00';
+const privateKey = '0x0000000000000000000000000000000000000000000000000000000000000001';
 const wallet = createWallet(privateKey);
 
 const executor = createExecutor({
