c29c0bfda11ac328df2aa2ba098832b607d963e6
wighawag <wighawag@gmail.com>
2023-06-01 19:30:53 +0100

    add nodejs-server

--- dreveal-worker/test/index.test.ts
+++
@@ -1,8 +0,0 @@
-import {expect, test} from 'vitest';
-import {router} from '../src/index';
-
-test('responds with url', async () => {
-	const req = new Request('http://localhost/');
-	const res = await router.handle(req);
-	expect(await res.text()).toBe('URL: http://localhost/ KEY: value');
-});
