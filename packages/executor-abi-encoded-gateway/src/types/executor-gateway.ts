84d1f87e261e3ac06b6fc975d19d3c2390c7045a
wighawag <wighawag@gmail.com>
2023-06-21 12:49:14 +0100

    remove abi-encoded gateway

--- packages/executor-abi-encoded-gateway/src/types/executor-gateway.ts
+++
@@ -1,7 +0,0 @@
-import type {TransactionInfo, TransactionSubmission} from 'fuzd-executor';
-
-export type SignedTransactionSubmission = TransactionSubmission & {signature: `0x${string}`};
-
-export type ExecutorGateway = {
-	submitSignedTransaction(id: string, submission: SignedTransactionSubmission): Promise<TransactionInfo>;
-};
