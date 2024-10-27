462afbacb0edd0f7bce4eaf7c1312a9001a96b03
wighawag <wighawag@gmail.com>
2024-10-27 20:58:39 +0000

    expose hono-client type from server

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -9,23 +9,25 @@ import {getSchedulingAPI} from './api/scheduling/index.js';
 import {getExecutionAPI} from './api/execution/index.js';
 import {setup} from './setup.js';
 import {getAdminDashboard} from './dashboard/admin/index.js';
+import {hc} from 'hono/client';
 
 export * from './storage/RemoteSQLExecutorStorage.js';
 export * from './storage/RemoteSQLSchedulerStorage.js';
 
 export function createServer<Env extends Bindings = Bindings>(options: ServerOptions<Env>) {
-	const app = new Hono<{Bindings: Env & {}}>()
-		.use(
-			'*',
-			cors({
-				origin: '*',
-				allowHeaders: ['X-Custom-Header', 'Upgrade-Insecure-Requests', 'Content-Type', 'SIGNATURE'],
-				allowMethods: ['POST', 'GET', 'HEAD', 'OPTIONS'],
-				exposeHeaders: ['Content-Length', 'X-Kuma-Revision'],
-				maxAge: 600,
-				credentials: true,
-			}),
-		)
+	const app = new Hono<{Bindings: Env & {}}>().use(
+		'*',
+		cors({
+			origin: '*',
+			allowHeaders: ['X-Custom-Header', 'Upgrade-Insecure-Requests', 'Content-Type', 'SIGNATURE'],
+			allowMethods: ['POST', 'GET', 'HEAD', 'OPTIONS'],
+			exposeHeaders: ['Content-Length', 'X-Kuma-Revision'],
+			maxAge: 600,
+			credentials: true,
+		}),
+	);
+
+	const home = new Hono<{Bindings: Env & {}}>()
 		// .use('*', async (ctx, next) => {
 		// 	console.log(ctx);
 		// 	await next();
@@ -40,17 +42,25 @@ export function createServer<Env extends Bindings = Bindings>(options: ServerOpt
 	const publicAPI = getPublicAPI<Env>(options);
 	const adminAPI = getAdminAPI<Env>(options);
 
-	const adminDashboard = getAdminDashboard<Env>(options);
-
-	return app
+	const api = new Hono<{Bindings: Env & {}}>()
 		.use('*', setup({serverOptions: options}))
-		.route('/api', publicAPI)
-		.route('/api/internal', internalAPI)
-		.route('/api/scheduling', schedulingAPI)
-		.route('/api/execution', executionAPI)
-		.route('/api/admin', adminAPI)
+		.route('/', publicAPI)
+		.route('/internal', internalAPI)
+		.route('/scheduling', schedulingAPI)
+		.route('/execution', executionAPI)
+		.route('/admin', adminAPI);
 
-		.route('/dashboard/admin', adminDashboard);
+	const adminDashboard = getAdminDashboard<Env>(options);
+	const dashboard = new Hono<{Bindings: Env & {}}>()
+		.use('*', setup({serverOptions: options}))
+		.route('/admin', adminDashboard);
+
+	return app.route('/', home).route('/api', api).route('/dashboard', dashboard);
 }
 
 export type App = ReturnType<typeof createServer<{}>>;
+
+// this is a trick to calculate the type when compiling
+const client = hc<App>('');
+export type Client = typeof client;
+export const createClient = (...args: Parameters<typeof hc>): Client => hc<App>(...args);
---
+++ tests/end-to-end/test/hono-client.test.ts
@@ -0,0 +1,37 @@
+import {describe, it, expect} from 'vitest';
+
+import {createClient} from 'fuzd-server';
+import {WORKER_URL} from './prool/node-instances';
+
+const client = createClient(WORKER_URL);
+
+describe('fuzd api', () => {
+	// --------------------------------------------------------------------------------------------
+	// wakup worker
+	// --------------------------------------------------------------------------------------------
+	it('startup', {timeout: 10000}, async () => {
+		await client.index.$get();
+	});
+	// --------------------------------------------------------------------------------------------
+
+	it(`fuzd-api`, async () => {
+		const publicKey = await (await client.api.publicKey.$get()).text();
+		expect(publicKey).toBeTypeOf('string');
+		expect(publicKey.length).toBeGreaterThan(0);
+	});
+
+	it(`schedule`, async () => {
+		const response = await client.api.scheduling.scheduleExecution.$post({
+			json: {
+				type: 'clear',
+				chainId: '0x1',
+				executions: [{}],
+				slot: '',
+				timing: {
+					type: 'fixed-time',
+					scheduledTime: 1000,
+				},
+			},
+		});
+	});
+});
