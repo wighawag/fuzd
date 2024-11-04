282712aa7952b84e497a1ff5acbbd02bb2c6462b
wighawag <wighawag@gmail.com>
2024-11-04 14:18:55 +0000

    add descriptions to openapi spec

---
+++ openapi-transform.js
@@ -0,0 +1,22 @@
+import fs from 'node:fs';
+
+const openapiSTR = fs.readFileSync('./packages/server/doc/openapi.json', 'utf-8');
+const openapi = JSON.parse(openapiSTR);
+
+const descriptions = JSON.parse(fs.readFileSync('./openapi-descriptions.json'));
+
+const methods = ['get', 'post'];
+
+const paths = Object.keys(openapi.paths);
+for (const path of paths) {
+	const description = descriptions[path];
+	if (description) {
+		for (const method of methods) {
+			if (openapi.paths[path][method]) {
+				openapi.paths[path][method].description = description[method];
+			}
+		}
+	}
+}
+
+fs.writeFileSync('./docs/public/openapi.json', JSON.stringify(openapi, null, 2));
